9bc207ab16dcd3fce832b867f62739702f062b04
bibia <134613103+BIALINDAA@users.noreply.github.com>
2024-08-22 12:16:26 -0300

    Add files via upload

--- aula04.js
+++ aula04.js
@@ -24,7 +24,7 @@ function some(){
     return 2 + 2
 }
 
-console.log(soma())
+//console.log(soma())
 imprimeTexto(soma())
 imprimeTexto(somaDeNumeros(4, 7))
 imprimeTexto(somaDeNumeros(24, 11))
@@ -47,3 +47,29 @@ function multiplicacao(numero4 = 2, numero5 = 5){
 }
 
 imprimeTexto(multiplicacao(somaDeNumeros(3,4), somaDeNumeros(5,10)));
+
+// expressão de função
+
+const constanteSoma =  function(nume1, nume2){return nume1 + nume2}
+
+console.log(constanteSoma(3,6))
+
+// Funções e var são "Listadas" no topo do arquivo
+
+// Arrow function
+
+const apresentaArrow = nome => `meu nome é ${nome}`;
+const somaEmFlecha = (nu1, nu2) => nu1 + nu2;
+
+console.log(apresentaArrow("Bia"))
+console.log(somaEmFecha(2,4))
+
+const somaNumerosPequenos = (num1, num2) => {
+    if(num1 || num2 > 10){
+        return "Essa função é apenas para números pequenos"
+    } else {
+        return num1 + num2;
+    }
+}
+
+console.log(somaNumerosPequenos(2,3))
